ce5cb0d62ee6050edf03ef30d45bde9e07e8718a
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-21 09:25:00 +0200

    Add project files.

---
+++ DMB0001v4/EchoState.cs
@@ -0,0 +1,10 @@
+﻿namespace DMB0001v4
+{
+    /// <summary>
+    /// Class for storing conversation state. 
+    /// </summary>
+    public class EchoState
+    {
+        public int TurnCount { get; set; } = 0;
+    }
+}
